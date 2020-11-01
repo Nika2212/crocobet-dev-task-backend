eae1dca68c762056ff6aa7769dd56fe9395c042d
nika2212 <nikabarsukovi@gmail.com>
2020-11-01 20:29:01 +0400

    Update

--- src/Core/Helpers/JWT.ts
+++ src/Core/Helpers/JWT.ts
@@ -5,5 +5,5 @@ export function generateToken(id: string, username: string): string {
   return jwt.sign({
     id: id,
     username: username
-  }, SECRET_KEY, {expiresIn: "1h"})
+  }, SECRET_KEY, {expiresIn: "2 days"})
 }
